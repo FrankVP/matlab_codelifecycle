e413b5c4e89476415a179df331ecded940f87aa8
Frank Van Puyvelde <Frank.VanPuyvelde@ymail.com>
2014-06-10 16:34:25 +0200

    added extra subfunction

--- HelloWorld.m
+++ HelloWorld.m
@@ -6,7 +6,13 @@ disp('Hello World');
 
 disp('Another Hello World');
 
-disp('Another Another Hello World');
+print_line(7);
+
+
+function print_line(x)
+
+    fprintf('%d is the number that was used as a parameter \n', x);
+end
 
 end
 
